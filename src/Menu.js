129bec03e1ec09e92224c67cda83add1279a2139
Dante Tobar <dante.m.tobar@gmail.com>
2024-07-13 08:18:26 -0700

    Use original menu styles

--- src/Menu.js
+++ src/Menu.js
@@ -1,6 +1,8 @@
 import "./Menu.scss";
 
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import { useLocation, Link } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
 
 export default function Menu(props) {
@@ -10,7 +12,29 @@ export default function Menu(props) {
     setShowMenu(false);
   };
 
-  const SLIDE_IN = 0.125;
+  const [hoverPage, setHoverPage] = useState(null);
+
+  const location = useLocation();
+  useEffect(() => {
+    const key = location.pathname.split("/")[1];
+
+    switch (key) {
+      case "":
+        setHoverPage("home");
+        break;
+      case "photo":
+        setHoverPage("photo");
+        break;
+      case "about":
+        setHoverPage("about");
+        break;
+      default:
+        setHoverPage("home");
+        break;
+    }
+  }, [location]);
+
+  const SLIDE_IN = 0.2;
   const TEXT_IN = 0.025;
 
   return (
@@ -22,67 +46,57 @@ export default function Menu(props) {
           animate={{ height: "100vh", transition: { duration: SLIDE_IN } }}
           exit={{ height: "0vh", transition: { delay: TEXT_IN * 2 } }}
         >
-          <motion.div className="Menu_Grid">
-            <motion.div
-              className="Menu_Grid_Item"
-              initial={{ opacity: 0, x: 10 }}
-              animate={{
-                opacity: 1,
-                x: 0,
-                transition: { delay: SLIDE_IN + TEXT_IN },
-              }}
-              exit={{ opacity: 0, x: 10 }}
-            >
-              <Link to="/photo" className="Menu_Link" onClick={closeMenu}>
+          <motion.div
+            className="Menu__Pages"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, transition: { delay: SLIDE_IN + TEXT_IN } }}
+            exit={{ opacity: 0 }}
+          >
+              <Link
+                to="/"
+                className={`Menu_Link ${hoverPage === "home" ? "selected" : ""}`}
+                onClick={closeMenu}
+              >
+                <span className="Menu_Link_Text">home</span>
+              </Link>
+              <h1> • </h1>
+              <Link
+                to="/photo"
+                className={`Menu_Link ${hoverPage === "photo" ? "selected" : ""}`}
+                onClick={closeMenu}
+              >
                 <span className="Menu_Link_Text">photo</span>
               </Link>
-            </motion.div>
-            <motion.div
-              className="Menu_Grid_Item"
-              initial={{ opacity: 0 }}
-              animate={{
-                opacity: 0.8,
-                x: 0,
-              }}
-              exit={{ opacity: 0 }}
-            ></motion.div>
-            <motion.div
-              className="Menu_Grid_Item"
-              initial={{ opacity: 0, x: -10 }}
-              animate={{
-                opacity: 1,
-                x: 0,
-                transition: { delay: SLIDE_IN + TEXT_IN },
-              }}
-              exit={{ opacity: 0, x: -10 }}
+            <h1> • </h1>
+            <Link
+              to="/about"
+              className={`Menu_Link ${hoverPage === "about" ? "selected" : ""}`}
+              onClick={closeMenu}
             >
-              <Link to="/about" className="Menu_Link" onClick={closeMenu}>
-                <span className="Menu_Link_Text">about</span>
-              </Link>
-            </motion.div>
-            {/* <motion.div */}
-            {/*   className="Menu__Socials" */}
-            {/*   initial={{ opacity: 0, y: -20 }} */}
-            {/*   animate={{ */}
-            {/*     opacity: 1, */}
-            {/*     y: 0, */}
-            {/*     transition: { delay: SLIDE_IN + TEXT_IN + 0.025 }, */}
-            {/*   }} */}
-            {/*   exit={{ opacity: 0, y: -10 }} */}
-            {/* > */}
-            {/*   <a */}
-            {/*     href="https://www.instagram.com/dantistador/" */}
-            {/*     className="Menu__Socials_Link" */}
-            {/*   > */}
-            {/*     Instagram */}
-            {/*   </a> */}
-            {/*   <a */}
-            {/*     href="https://www.linkedin.com/in/dante-m-tobar/" */}
-            {/*     className="Menu__Socials_Link" */}
-            {/*   > */}
-            {/*     LinkedIn */}
-            {/*   </a> */}
-            {/* </motion.div> */}
+              <span className="Menu_Link_Text">about</span>
+            </Link>
+          </motion.div>
+          <motion.div
+            className="Menu__Socials"
+            initial={{ opacity: 0 }}
+            animate={{
+              opacity: 1,
+              transition: { delay: SLIDE_IN + TEXT_IN + 0.025 },
+            }}
+            exit={{ opacity: 0 }}
+          >
+            <a
+              href="https://www.instagram.com/dantistador/"
+              className="Menu__Socials_Link"
+            >
+              instagram
+            </a>
+            <a
+              href="https://www.linkedin.com/in/dante-m-tobar/"
+              className="Menu__Socials_Link"
+            >
+              linkedin
+            </a>
           </motion.div>
         </motion.div>
       )}
