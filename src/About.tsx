3591e34b07bfd3d955dd5fa6ec4a7982e70108a7
Dante Tobar <dante.m.tobar@gmail.com>
2025-01-03 11:09:00 -0800

    Refactor about with BEM syntax

--- src/About.tsx
+++ src/About.tsx
@@ -4,7 +4,64 @@ import { LinkedIn, Instagram, GitHub } from "@mui/icons-material";
 import { useEffect } from "react";
 import Riser from "./components/Riser";
 
-export default function About() {
+const Socials = () => {
+  return (
+    <div className="Socials">
+      <span className="Socials__topline" />
+      <div className="Socials__links">
+        <a
+          href="https://www.instagram.com/dantistador/"
+          target="_blank"
+          rel="noreferrer"
+          className="Socials--link"
+        >
+          <Instagram />
+        </a>
+        <a
+          className="Socials--link"
+          href="https://www.linkedin.com/in/dante-m-tobar/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <LinkedIn />
+        </a>
+        <a
+          className="Socials--link"
+          href="https://www.github.com/dqnte"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <GitHub />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+const Bio = () => {
+  return (
+    <div className="Bio">
+      <h1 className="Bio__name">Dante Tobar</h1>
+      <p className="Bio__text">
+        I'm an engineer & photographer. I shoot both film and digital. Most of
+        my photos are candids, but there are some posed exceptions. Currently, I
+        work at{" "}
+        <a
+          href="https://www.fabrichealth.com/about"
+          target="_blank"
+          rel="noreferrer"
+          className="Bio--link"
+        >
+          fabric
+        </a>{" "}
+        as a fullstack engineer. If you want to get in contact, feel free to dm
+        me.
+      </p>
+    </div>
+  );
+};
+
+const About = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -14,61 +71,18 @@ export default function About() {
       <div className={"About"}>
         <Riser>
           <div className="About__container">
-            {/* <img className="About__image" src="" /> */}
             <div className="About__image">
               <span>need to take this...</span>
             </div>
             <div className="About__text">
-              <div className="About__bio">
-                <h1 className="About__name">Dante Tobar</h1>
-                <p>
-                  I'm an engineer & photographer. I shoot both film and digital.
-                  Most of my photos are candids, but there are some posed
-                  exceptions. Currently, I work at{" "}
-                  <a
-                    href="https://www.fabrichealth.com/about"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    fabric
-                  </a>{" "}
-                  as a fullstack engineer. If you want to get in contact, feel
-                  free to dm me.
-                </p>
-              </div>
-              <div className="About__socials">
-                <span className="About__socials_topline" />
-                <div className="About__socials_links">
-                  <a
-                    href="https://www.instagram.com/dantistador/"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="About__social_link"
-                  >
-                    <Instagram />
-                  </a>
-                  <a
-                    className="About__social_link"
-                    href="https://www.linkedin.com/in/dante-m-tobar/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <LinkedIn />
-                  </a>
-                  <a
-                    className="About__social_link"
-                    href="https://www.github.com/dqnte"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <GitHub />
-                  </a>
-                </div>
-              </div>
+              <Bio />
+              <Socials />
             </div>
           </div>
         </Riser>
       </div>
     </AnimatePresence>
   );
-}
+};
+
+export default About;
